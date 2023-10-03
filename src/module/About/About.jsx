65488c162e1d01f082127dcd55c13ef91551a776
Anil Adhikari <adhikarianil719@gmail.com>
2023-10-03 22:52:31 +0545

    About.jsx file is updated..

--- src/module/About/About.jsx
+++ src/module/About/About.jsx
@@ -7,7 +7,7 @@ function About() {
         <div class="flex flex-col space-y-8 pb-10 pt-12 md:pt-24">
           <div class="max-w-max rounded-full border bg-gray-50 p-1 px-3">
             <p class="text-xs font-semibold leading-normal md:text-sm">
-              About the company
+              About our company
             </p>
           </div>
           <p class="text-3xl font-bold text-gray-900 md:text-5xl md:leading-10">
